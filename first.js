5c21f9161544cbdf732b0accbbf88bf8f0dd9a76
Nikita Bhujel <nikibhujel1@gmail.com>
2025-05-02 17:13:18 +0545

    Javascript variables, data types

---
+++ first.js
@@ -0,0 +1,31 @@
+console.log('Nikita');
+//variable declaration
+fullname="Nikita Bhujel"
+console.log('fullname');
+console.log(fullname);
+age=24;
+console.log(age);
+price=99.99;
+console.log(price);
+x=null;
+console.log(x);
+y=undefined;
+console.log(y); 
+//Data Types
+isFollow=true;
+console.log(isFollow);
+//objects
+const student={
+    Fullname:"N B",
+    age:20,
+    cgpa:8.9,
+    isPass:true,
+
+};
+//accessing
+console.log(student["age"]);
+console.log(student.Fullname);  
+student["age"]=student["age"]+1;
+console.log(student["age"]);
+
+
