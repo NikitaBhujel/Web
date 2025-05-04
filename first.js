44dedfdce937c8cfe20cf0c6f090c6e2114b21ef
Nikita Bhujel <nikibhujel1@gmail.com>
2025-05-04 21:04:42 +0545

    Assignment Operators

--- first.js
+++ first.js
@@ -27,6 +27,7 @@ console.log(student["age"]);
 console.log(student.Fullname);  
 student["age"]=student["age"]+1;
 console.log(student["age"]);
+
 //Arithmetic operators
 let a=5;
 let b=2;
@@ -43,11 +44,31 @@ console.log("a**b=",a**b); //25
 //Unary Operator
 let n=3;
 let s=9;
-// console.log("n=",n,"s=",s);
-// // n=n+1;
-// n++;
-// n--;
-// // console.log("n=",n); 
-// console.log("++a=",++a);
-// console.log("--a=",--a);
+console.log("n=",n,"s=",s);
+// n=n+1;
+++n;
+n--;
+console.log("n=",n); 
+console.log("++a=",++a);
+console.log("--a=",--a);
 console.log("a--=",a--);
+
+//Assignment Operators
+let d=5;
+// d %=4;
+// console.log("d=",d);  //1
+d **= 2; //d=d**2
+console.log("d=",d) ;  
+
+// comparison operators
+let e=5;
+let f=2;
+console.log("5==2",e==f); 
+let g=5; //number
+let h="5"; //string
+console.log("g==h",g==h); 
+let i=5; //number
+let j="5"; //string
+console.log("i===j",i===j);
+
+ 
