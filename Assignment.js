3491b84704aef988813bbb538ab3abe7762d4ac6
Nikita Bhujel <nikibhujel1@gmail.com>
2025-05-05 16:02:37 +0545

    Strings,methods,Properties

---
+++ Assignment.js
@@ -0,0 +1,3 @@
+let input=prompt("Enter your fullname without spaces");
+let Username="@"+input+input.length;
+console.log("USername:",Username);
--- loops&strings.js
+++ loops&strings.js
@@ -76,11 +76,57 @@ for(let e=0;e<=100;e++)
 
     // console.log(e,e%2==0,);
 }
-let game_number=9;
-let gnumber=prompt("Enter your guess number:");
-while(gnumber!=game_number) //Here if we use double equals to, we cant get the ans because prompe returns string but we are giving integer value
-{
-    gnumber=prompt("You entered wrong number.Guess again:");
-}
-console.log("Congratulations,you entered the right number.");
+// let game_number=9;
+// let gnumber=prompt("Enter your guess number:");
+// while(gnumber!=game_number) //Here if we use double equals to, we cant get the ans because prompe returns string but we are giving integer value
+// {
+//     gnumber=prompt("You entered wrong number.Guess again:");
+// }
+// console.log("Congratulations,you entered the right number.");
+
+//strings
+ let st="Nikita"; //create string
+ //string length -> str.length
+ //string indices ->str[0],str[2];
+ console.log(st[0]);
+ //Templaten Literals
+ let sentence=`This is a templete literal.`;
+ console.log(sentence);
+
+ let obj={
+    item: "copy",
+    price: 9,
+ };
+ console.log("The cost of",obj.item,"is",obj.price,"rupees");
+
+ let output=`The cost of ${obj.item} is ${obj.price} rupees`; //template literals
+ console.log(output);
+
+ let tem=`I am ${10+10+1} years old.`;
+ console.log(tem);
+
+ console.log("Mero desh");
+ console.log("mero\ndesh"); // \t is another escape character
+let a="mero\tnaam";
+console.log(a.length); // \t is counted as 1 so total length counts 9
+
+//string methods
+let b='Niki';
+let c=str.toLocaleUpperCase();
+console.log(c);
+
+let s="         Loops and   strings   ";
+console.log(s.trim());
+let t="01234567";
+console.log(t.slice(1,6))
+console.log(t.slice(1));
+
+console.log(s.concat(t));
+let res="hello"+123; //Here number is converted to string then concatenated
+console.log(res);
+console.log(res.charAt(3));
+
+let rr="IloveJS";
+rr=rr.replace("I","S");
+console.log(rr);
 
